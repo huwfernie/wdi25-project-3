1c0ea7fade58dc256b92607abb0e51f2c24e80e5
Muge Ersoz <muge@ersoz.org>
2017-03-25 13:27:27 +0000

    Set up track controller

---
+++ controllers/tracks.js
@@ -0,0 +1,73 @@
+const Track = require('../models/track');
+
+function indexRoute(req, res, next) {
+  Track
+    .find()
+    .exec()
+    .then((tracks) => res.json(tracks))
+    .catch(next);
+}
+
+function createRoute(req, res, next) {
+  // if(req.file) req.body.start.img = req.file.filename;
+  // image files -- do with imageupload and base64 directive etc.
+
+  req.body.createdBy = req.user;
+
+  Track
+    .create(req.body)
+    .then((track) => res.status(201).json(track))
+    .catch(next);
+}
+
+function showRoute(req, res, next) {
+  Track
+    .findById(req.params.id)
+    .exec()
+    .then((track) => {
+      if(!track) return res.notFound();
+
+      res.json(track);
+    })
+    .catch(next);
+}
+
+function updateRoute(req, res, next) {
+  // if(req.file) req.body.image = req.file.filename;
+
+  Track
+    .findById(req.params.id)
+    .exec()
+    .then((track) => {
+      if(!track) return res.notFound();
+
+      for(const field in req.body) {
+        track[field] = req.body[field];
+      }
+
+      return track.save();
+    })
+    .then((track) => res.json(track))
+    .catch(next);
+}
+
+function deleteRoute(req, res, next) {
+  Track
+    .findById(req.params.id)
+    .exec()
+    .then((track) => {
+      if(!track) return res.notFound();
+
+      return track.remove();
+    })
+    .then(() => res.status(204).end())
+    .catch(next);
+}
+
+module.exports = {
+  index: indexRoute,
+  create: createRoute,
+  show: showRoute,
+  update: updateRoute,
+  delete: deleteRoute
+};
