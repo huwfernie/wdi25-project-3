0c909b4050f4180815dea1757e063a1a8df9aacb
tjallen27 <tjallen91@hotmail.co.uk>
2017-03-25 08:15:26 +0000

    Added favedBy and AttemptedBy to track schema

--- models/track.js
+++ models/track.js
@@ -2,16 +2,21 @@ const mongoose = require('mongoose');
 
 const trackSchema = new mongoose.Schema({
   start: {
+    name: { type: String, required: true },
     lat: { type: Number, required: true },
     lng: { type: Number, required: true },
-    img: { type: String, required: true }
+    img: { type: String, required: true },
+    distance: { type: String }
   },
   finish: {
+    name: { type: String, required: true },
     lat: { type: Number, required: true },
     lng: { type: Number, required: true },
     img: { type: String, required: true }
   },
-  distance: { type: Number }
+  distance: { type: String },
+  attemptedBy: [],
+  favedBy: []
 });
 
 module.exports = mongoose.model('Track', trackSchema);
