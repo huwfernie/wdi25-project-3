e5915ccdaf5ce5b2d96fc8211a900d7ae47b95db
tjallen27 <tjallen91@hotmail.co.uk>
2017-03-25 07:53:49 +0000

    Start building out track schema

--- models/track.js
+++ models/track.js
@@ -2,15 +2,16 @@ const mongoose = require('mongoose');
 
 const trackSchema = new mongoose.Schema({
   start: {
-    lat: {type: Number, required: true},
-    lng: {type: Number, required: true},
-    img: {type: String, required: true}
+    lat: { type: Number, required: true },
+    lng: { type: Number, required: true },
+    img: { type: String, required: true }
   },
   finish: {
-    lat: {type: Number, required: true},
-    lng: {type: Number, required: true},
-    img: {type: String, required: true}
-  }
+    lat: { type: Number, required: true },
+    lng: { type: Number, required: true },
+    img: { type: String, required: true }
+  },
+  distance: { type: Number }
 });
 
 module.exports = mongoose.model('Track', trackSchema);
