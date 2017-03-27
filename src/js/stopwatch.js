136a98eadc7e2cbb5d83c666d89e5661bfb5259b
Muge Ersoz <muge@ersoz.org>
2017-03-27 16:17:07 +0100

    Added stopwatch in sprint show and stopwatch js function

---
+++ src/js/stopwatch.js
@@ -0,0 +1,43 @@
+window.onload = function buildStopwatch() {
+  var stopwatch = document.getElementById('stopwatch'),
+    start = document.getElementById('start'),
+    stop = document.getElementById('stop'),
+    clear = document.getElementById('clear'),
+    seconds = 0, minutes = 0, hours = 0,
+    t;
+
+  function add() {
+    seconds++;
+    if (seconds >= 60) {
+      seconds = 0;
+      minutes++;
+      if (minutes >= 60) {
+        minutes = 0;
+        hours++;
+      }
+    }
+
+    stopwatch.textContent = (hours ? (hours > 9 ? hours : '0' + hours) : '00') + ':' + (minutes ? (minutes > 9 ? minutes : '0' + minutes) : '00') + ':' + (seconds > 9 ? seconds : '0' + seconds);
+    timer();
+  }
+
+  function timer() {
+    t = setTimeout(add, 1000);
+  }
+
+  // timer();
+
+
+  /* Start button */
+  start.onclick = timer;
+
+  /* Stop button */
+  stop.onclick = function() {
+    clearTimeout(t);
+  };
+
+  /* Clear button */
+  clear.onclick = function() {
+    stopwatch.textContent = (hours ? (hours > 9 ? hours : '0' + hours) : '00') + ':' + (minutes ? (minutes > 9 ? minutes : '0' + minutes) : '00') + ':' + (seconds > 9 ? seconds : '0' + seconds);  seconds = 0; minutes = 0; hours = 0;
+  };
+};
