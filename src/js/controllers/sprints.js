7c4f21777d5b0a2d9f77090ef70f88b30ef8aebb
Huw Fernie <HuwFernie@gmail.com>
2017-03-29 12:30:38 +0100

    changed the sprint controller http request to get goolge vision working again

--- src/js/controllers/sprints.js
+++ src/js/controllers/sprints.js
@@ -19,6 +19,7 @@ function SprintsNewCtrl(Sprint, $state, $http, trackService) {
 
   const track = trackService.getTrack();
 
+
   if(!track) {
     $state.go('tracksIndex'); // handle this error more gracefully with $broadcast or something...
   }
@@ -32,25 +33,29 @@ function SprintsNewCtrl(Sprint, $state, $http, trackService) {
       }]
     };
 
-    $http({
-      url: 'https://vision.googleapis.com/v1/images:annotate',
-      data,
-      params: { key: 'AIzaSyBWXs5eQMEdN8caiQ-9QO0GqxRifMqNPyU' }
-    })
-    .then((response) => {
-      const latLng = response.data.responses[0].landmarkAnnotations[0].locations[0].latLng;
 
-      if(track.start.lat !== latLng.latitude || track.start.longitude !== latLng.lng) {
-        return false; // handle this error more gracefully with $broadcast or something...
-      }
 
-      vm.sprint.track = track;
+    $http
+      .post('https://vision.googleapis.com/v1/images:annotate?key=AIzaSyBWXs5eQMEdN8caiQ-9QO0GqxRifMqNPyU', data)
+      .then((response) => {
+        console.log('response', response);
+        const latLng = response.data.responses[0].landmarkAnnotations[0].locations[0].latLng;
 
-      Sprint
-        .save(vm.sprint)
-        .$promise
-        .then((sprint) => $state.go('sprintsShow', { id: sprint.id }));
-    });
+        if(track.track.start.lat !== latLng.latitude || track.track.start.lng !== latLng.longitude) {
+          console.log('false - you\'re start photo doesn\'t match');
+          //return false; // handle this error more gracefully with $broadcast or something...
+        }
+
+        console.log('now');
+        vm.sprint.track = track;
+        // console.log(track);
+        // console.log(response);
+
+        Sprint
+          .save(vm.sprint)
+          .$promise
+          .then((sprint) => $state.go('sprintsShow', { id: sprint.id }));
+      });
   }
 
 
