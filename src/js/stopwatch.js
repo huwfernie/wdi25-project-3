ed1b7e06e9c0de4fde619f323ece183a3e61bb75
Huw Fernie <HuwFernie@gmail.com>
2017-03-29 16:46:00 +0100

    got rid of stopwatch console error

--- src/js/stopwatch.js
+++ src/js/stopwatch.js
@@ -29,15 +29,16 @@ window.onload = function buildStopwatch() {
 
 
   /* Start button */
-  start.onclick = timer;
+  if(start) start.onclick = timer ;
+
 
   /* Stop button */
-  stop.onclick = function() {
+  if(stop) stop.onclick = function() {
     clearTimeout(t);
   };
 
   /* Clear button */
-  clear.onclick = function() {
+  if(clear) clear.onclick = function() {
     stopwatch.textContent = (hours ? (hours > 9 ? hours : '0' + hours) : '00') + ':' + (minutes ? (minutes > 9 ? minutes : '0' + minutes) : '00') + ':' + (seconds > 9 ? seconds : '0' + seconds);  seconds = 0; minutes = 0; hours = 0;
   };
 };
